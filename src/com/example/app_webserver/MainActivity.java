7a73e3a1953fc4580650168a600b5d9856fc878a
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2014-10-09 20:30:11 -0300

    agregado
    funcionando

--- src/com/example/app_webserver/MainActivity.java
+++ src/com/example/app_webserver/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.app_webserver;
 
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -12,25 +12,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        httpHandler handler =new httpHandler();
+        String txt = handler.post("http://192.168.0.5/app_android/android.php");
+        TextView t =(TextView) findViewById(R.id.text1);
+        t.setText(txt);
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+  
 }
---
+++ src/com/example/app_webserver/httpHandler.java
@@ -0,0 +1,29 @@
+package com.example.app_webserver;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class httpHandler {
+	
+	public String post(String posturl){
+		try {
+			HttpClient httpclient =new DefaultHttpClient();
+			HttpPost httppost =new HttpPost(posturl);
+			HttpResponse resp =httpclient.execute(httppost);
+			HttpEntity ent =resp.getEntity();
+			String text=EntityUtils.toString(ent);
+			
+			return text;
+			
+		} catch (Exception e) {
+			return "error";
+		}
+		
+		
+	}
+
+}
